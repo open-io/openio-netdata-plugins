0f2f762893292b5792403caeed1194226fe27adc
Vladimir DOMBROVSKI <vladimir@openio.io>
2017-11-23 01:35:11 +0100

    Make collection work (still WIP)

--- collect.go
+++ collect.go
@@ -9,85 +9,31 @@ import (
 	"log"
 	"io/ioutil"
 	"encoding/json"
-	"strconv"
-	//"time"
+	"time"
+	"flag"
 )
 
 // BasePath -- base configuration path
 var BasePath = "/tmp/sds.conf.d/";
 
 // PollsBeforeReload -- Number of cycles before collector is reloaded
+// Restarting the collector every now and then should help getting rid of memleaks
 var PollsBeforeReload = 1000;
 
 // PollInterval -- Seconds to wait between cycles
 var PollInterval = 10;
 
-func main() {
-	ns := "OPENIO";
-	proxyURL, err := getProxyURL(ns)
-	raiseIf(err)
-
-	// lastOp := time.Now()
-	// elapsed := 0
-	// now := time.Now()
-	//createCharts(reqServices("rawx", proxyURL, ns))
-	sTypes := reqInfo(proxyURL, ns)
-
-	serviceList := reqServices(sTypes, proxyURL, ns)
-	fmt.Printf("%+v\n", serviceList);
-	return
-
-	//poll := 0;
-
-	// for poll < PollsBeforeReload {
-	// 	serviceList := reqServices(sTypes, proxyURL, ns)
-	// 	now = time.Now()
-	// 	elapsed = int(now.Sub(lastOp) / 1000)
-	// 	lastOp = now
-	//
-	// 	// Update charts
-	// 	// fmt.Println(res2, elapsed, sTypes)
-	//
-	// 	for i := range serviceList {
-	// 		updateChart(elapsed, serviceList[i].Addr, serviceList[i].Score)
-    // 	}
-	//
-	// 	time.Sleep(time.Duration(PollInterval) * 1000 * time.Millisecond)
-	// 	poll++;
-	// }
-
-}
-
-// Initialize charts
-func createCharts(serviceList ServiceInfo) {
-	fmt.Println("CHART openio.score '' 'OpenIO Service score' 'score'")
-	for i := range serviceList {
-		fmt.Printf("DIMENSION %s '' absolute\n", serviceList[i].Addr)
-	}
-	// fmt.Println("DIMENSION service '' absolute")
-	// fmt.Println("DIMENSION score '' absolute")
-	// for _, v := range sTypes {
-	// 	fmt.Printf("DIMENSION %s '' absolute\n", v)
-	// }
-}
-
-func updateChart(elapsed int, service string, value int) {
-	fmt.Printf("BEGIN openio.score %d\n", elapsed)
-	fmt.Printf("SET %s %d\n", service, value)
-	fmt.Println("END")
-}
-
-func makeRequest(url string) (string) {
-	resp, err := http.Get(url);
-	raiseIf(err)
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	raiseIf(err)
-	return string(body)
-}
+// DimPrefix -- prefix to add to dimensions
+var DimPrefix = "openio"
 
 // ServiceType -- list of service types
 type ServiceType []string
+
+// Charts -- list of already created charts with the dimensions
+var Charts = make(map[string][]string)
+
+// Elapsed -- Time elapsed since last update
+//var Elapsed = 0
 
 // ServiceInfo -- decoded service metric information
 type ServiceInfo []struct {
@@ -98,62 +44,33 @@ type ServiceInfo []struct {
 		Loc string
 		Vol string
 	}
-	Metax map[string]int64
-	Rawx map[string]int64
-	Volume string
 }
 
-// ServiceData -- assembled info of all service types
-type ServiceData map[string]ServiceInfo
+func main() {
+	nsPtr := flag.String("ns", "OPENIO", "List of namespaces delimited by semicolons (:)")
+	intervalPtr := flag.Int("interval", 10, "Update every x seconds")
+	flag.Parse()
 
+	PollInterval = *intervalPtr;
 
-
-
-func reqInfo(proxyURL string, ns string) (ServiceType) {
-	url := fmt.Sprintf("http://%s/v3.0/%s/conscience/info?what=types", proxyURL, ns)
-	res := ServiceType{}
-	json.Unmarshal([]byte(makeRequest(url)), &res)
-	return res
-}
-
-func parseRawxCounters(body string) (map[string]int64, string) {
-	res := make(map[string]int64)
-	volume := ""
-	var lines = strings.Split(body, "\n");
-	for i := range lines {
-		s := strings.Split(lines[i], " ")
-		if s[0] == "counter" {
-			v, err := strconv.ParseInt(s[2], 10, 64)
-			raiseIf(err)
-			res[s[1]] = v
-		} else if s[1] == "volume" {
-			volume = s[2]
-		}
+	var proxyURLs = make(map[string]string)
+	var namespaces = strings.Split(*nsPtr, ":")
+	for i := range namespaces {
+		proxyURLs[namespaces[i]] = getProxyURL(namespaces[i]);
 	}
-	return res, volume
-}
 
-func reqServices(serviceType ServiceType, proxyURL string, ns string) (ServiceData) {
-	res := ServiceData{}
-	for i := range serviceType {
-		serviceInfo := ServiceInfo{}
-		fmt.Println(serviceType[i])
-		url := fmt.Sprintf("http://%s/v3.0/%s/conscience/list?type=%s", proxyURL, ns, serviceType[i])
-		json.Unmarshal([]byte(makeRequest(url)), &serviceInfo)
+	poll := 0
+	//last := time.Now()
 
-		if serviceType[i] == "rawx" {
-			for j := range serviceInfo {
-				url := fmt.Sprintf("http://%s/stat", serviceInfo[j].Addr)
-				serviceInfo[j].Rawx, serviceInfo[j].Volume = parseRawxCounters(makeRequest(url))
-				// serviceInfo[j].Rawx = rawxInfo
-				// serviceInfo[j].Volume = volume
-			}
+	for poll < PollsBeforeReload {
+		// Elapsed = int(time.Now().Sub(last) / 1000)
+		// last = time.Now()
+		for ns, proxyURL := range proxyURLs {
+			updateServices(proxyURL, ns);
 		}
-
-		res[serviceType[i]] = serviceInfo
-
+		time.Sleep(time.Duration(PollInterval) * 1000 * time.Millisecond);
+		poll++;
 	}
-	return res
 }
 
 func raiseIf(err error) {
@@ -162,7 +79,34 @@ func raiseIf(err error) {
 	}
 }
 
-func getProxyURL(ns string) (string, error) {
+func keyInMap(s string, m map[string][]string) bool {
+	for k := range m {
+        if k == s {
+            return true
+        }
+    }
+    return false
+}
+
+func itemInList(s string, list []string) bool {
+    for i := range list {
+        if list[i] == s {
+            return true
+        }
+    }
+    return false
+}
+
+func httpGet(url string) string {
+	resp, err := http.Get(url);
+	raiseIf(err)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	raiseIf(err)
+	return string(body)
+}
+
+func getProxyURL(ns string) string {
   file, err := os.Open(BasePath + ns)
   raiseIf(err)
   defer file.Close()
@@ -171,8 +115,102 @@ func getProxyURL(ns string) (string, error) {
   for scanner.Scan() {
 	t := scanner.Text()
 	if strings.HasPrefix(t, "proxy") {
-		return strings.Split(t, "=")[1], nil;
+		return strings.Split(t, "=")[1];
 	}
   }
-  return "", scanner.Err()
+  raiseIf(scanner.Err())
+  return ""
+}
+
+func createChart(chart string, desc string, title string, units string) {
+	fmt.Printf("CHART %s '%s' '%s' '%s'\n", chart, desc, title, units)
+}
+
+func createDim(dim string) {
+	fmt.Printf("DIMENSION %s '' absolute\n", dim)
+}
+
+func updateChart(chart string, dim string, value string) {
+	chart = fmt.Sprintf("%s.%s", DimPrefix, strings.Replace(chart, ".", "_", -1))
+	//dim = strings.Replace(dim, ".", "_", -1)
+	chartTitle := strings.ToUpper(strings.Join(strings.Split(chart, "_"), " "))
+	if !keyInMap(chart, Charts) {
+		createChart(chart, "", chartTitle, "")
+		Charts[chart] = make([]string, 0)
+	}
+	if !itemInList(dim, Charts[chart]) {
+		createChart(chart, "", chartTitle, "")
+		createDim(dim)
+		Charts[chart] = append(Charts[chart], dim)
+	}
+
+	//fmt.Printf("BEGIN %s %d\n", chart, Elapsed)
+	fmt.Printf("BEGIN %s\n", chart)
+	fmt.Printf("SET %s %s\n", dim, value)
+	fmt.Println("END")
+}
+
+func getServiceTypes(proxyURL string, ns string) ServiceType {
+	url := fmt.Sprintf("http://%s/v3.0/%s/conscience/info?what=types", proxyURL, ns)
+	res := ServiceType{}
+	json.Unmarshal([]byte(httpGet(url)), &res)
+	return res
+}
+
+func updateRawxCounters(ns string, service string) {
+	url := fmt.Sprintf("http://%s/stat", service)
+	var lines = strings.Split(httpGet(url), "\n");
+	for i := range lines {
+		s := strings.Split(lines[i], " ")
+		if s[0] == "counter" {
+			updateChart(s[1], fmt.Sprintf("%s@%s", service, ns), s[2])
+		} else if s[1] == "volume" {
+			// TODO: do something with volume?
+		}
+	}
+}
+
+func updateMetaxCounters(ns string, service string, proxyURL string) {
+	url := fmt.Sprintf("http://%s/v3.0/forward/stats?id=%s", proxyURL, service)
+	var lines = strings.Split(httpGet(url), "\n");
+	for i := range lines {
+		s := strings.Split(lines[i], " ")
+		if s[0] == "counter" {
+			updateChart(s[1], fmt.Sprintf("%s@%s", service, ns), s[2])
+		} else if s[1] == "volume" {
+			// TODO: do something with volume?
+		} else if s[1] == "gauge" {
+			// TODO: do something with gauge?
+		}
+	}
+}
+
+func updateScore(proxyURL string, ns string, serviceType string) ServiceInfo {
+	serviceInfo := ServiceInfo{}
+	url := fmt.Sprintf("http://%s/v3.0/%s/conscience/list?type=%s", proxyURL, ns, serviceType)
+	json.Unmarshal([]byte(httpGet(url)), &serviceInfo)
+	for i := range serviceInfo {
+		updateChart("score", fmt.Sprintf("%s@%s", serviceInfo[i].Addr, ns), fmt.Sprint(serviceInfo[i].Score))
+	}
+	return serviceInfo
+}
+
+func updateServices(proxyURL string, ns string) {
+	var serviceType = getServiceTypes(proxyURL, ns)
+	for t := range serviceType  {
+		var serviceInfo = updateScore(proxyURL, ns, serviceType[t])
+		if serviceType[t] == "rawx" {
+			for sc := range serviceInfo {
+				if strings.HasPrefix(serviceInfo[sc].Addr, strings.Split(proxyURL, ":")[0]) {
+					updateRawxCounters(ns, serviceInfo[sc].Addr)
+				}
+			}
+		} else if strings.HasPrefix(serviceType[t], "meta") {
+			for sc := range serviceInfo {
+				if strings.HasPrefix(serviceInfo[sc].Addr, strings.Split(proxyURL, ":")[0]) {
+					updateMetaxCounters(ns, serviceInfo[sc].Addr, proxyURL)
+				}
+			}
+		}
+	}
 }
