0dbebfe54b49d1fb0dfdaab8289d053d4878c67a
Vladimir Dombrovski <vladimir@openio.io>
2020-07-10 13:34:46 +0200

    Container plugin: allow separate bucketDB

--- cmd/container.plugin/container.plugin.go
+++ cmd/container.plugin/container.plugin.go
@@ -39,11 +39,13 @@ func main() {
 	var limit int64
 	var threshold int64
 	var fast bool
+	var bucketdb string
 
 	fs := flag.NewFlagSet("", flag.ExitOnError)
 	fs.StringVar(&ns, "ns", "OPENIO", "List of namespaces delimited by semicolons (:)")
 	fs.StringVar(&conf, "conf", "/etc/oio/sds/", "Path to SDS config directory")
 	fs.StringVar(&addr, "addr", "", "Force redis IP:PORT for each namespace")
+	fs.StringVar(&bucketdb, "bucketdb", "", "BucketDB redis IP:PORT for each namespace")
 	fs.Int64Var(&limit, "limit", -1, "Amount of processed containers in a single request, -1 for unlimited")
 	fs.Int64Var(&threshold, "threshold", 3e5, "Minimal number of objects in container to report it")
 	fs.BoolVar(&fast, "fast", false, "Use fast account listing")
@@ -55,10 +57,10 @@ func main() {
 
 	namespaces := strings.Split(ns, ":")
 	redisAddr := strings.Split(addr, ",")
-	collector.Run(intervalSeconds, makeCollect(conf, redisAddr, namespaces, limit, threshold, fast))
+	collector.Run(intervalSeconds, makeCollect(conf, redisAddr, strings.Split(bucketdb, ","), namespaces, limit, threshold, fast))
 }
 
-func makeCollect(basePath string, addr, namespaces []string, l int64, t int64, f bool) (collect collector.Collect) {
+func makeCollect(basePath string, addr, bucketdb, namespaces []string, l int64, t int64, f bool) (collect collector.Collect) {
 
 	return func(c chan netdata.Metric) error {
 		errors := make(map[string]error)
@@ -75,7 +77,15 @@ func makeCollect(basePath string, addr, namespaces []string, l int64, t int64, f
 				}
 			}
 			client := redis.NewClient(&redis.Options{Addr: redisAddr})
-			errors[ns] = container.Collect(client, ns, l, t, f, c)
+
+			var bucketDBClient *redis.Client
+			if i < len(bucketdb) && bucketdb[i] != "" {
+				bucketDBClient = redis.NewClient(&redis.Options{Addr: bucketdb[i]})
+			} else {
+				bucketDBClient = client
+			}
+
+			errors[ns] = container.Collect(client, bucketDBClient, ns, l, t, f, c)
 		}
 		for _, err := range errors {
 			if err != nil {
